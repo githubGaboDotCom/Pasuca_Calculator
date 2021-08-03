b8e5253e79f86e27699cf5eef14fc3107caeecd0
githubGaboDotCom <completamentesud@gmail.com>
2021-08-03 12:05:20 -0600

    finished the search bar, modified the main page to display selected products

--- calculator_features.js
+++ calculator_features.js
@@ -1,3 +1,35 @@
-function displayDropDown() {
-    document.getElementById("DropDown_Menu").classList.toggle("show");
+const search_Menu = document.querySelector(".Style_Menu");
+const inputBox = search_Menu.querySelector("input");
+const optionBox = search_Menu.querySelector(".Style_For_productOptions");
+
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value;
+    let emptyArray = [];
+    if (userData) {
+        emptyArray = options.filter((data)=>{
+            return data.toLocaleLowerCase().indexOf(userData.toLocaleLowerCase()) > -1;
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>' + data + '<li>';
+        });
+        console.log(emptyArray);
+        search_Menu.classList.add("active");
+    }else{
+        search_Menu.classList.remove("active");
+
+    }
+    showOptions(emptyArray);
+}
+
+function showOptions (list) {
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = userValue;
+
+    }else {
+        listData = list.join('');
+    }
+    optionBox.innerHTML = listData;
+
 }
