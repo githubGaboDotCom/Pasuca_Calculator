38cb242b20750eab79917761ea53764baf19519e
githubGaboDotCom <completamentesud@gmail.com>
2021-08-07 05:19:36 -0600

    adding math features to the calculator, still working on it

--- calculator_features.js
+++ calculator_features.js
@@ -29,16 +29,37 @@ inputBox.onkeyup = (e)=>{
 const table_query = document.getElementById("table");
 const tr_query = table_query.getElementsByTagName("tr")[1];
 const td_query = tr_query.getElementsByTagName("td")[0];
+const td_query_price = tr_query.getElementsByTagName("td")[1];
+const td_query_total = tr_query.getElementsByTagName("td")[4];
+const td_query_input = tr_query.getElementsByTagName("td")[2];
+const td_query_value = td_query_input.querySelector("input");
+var price;
 
 
+function extractNumber (productName) {
+    var priceValue = productName.replace(/[^0-9]/g, '');
+    var price = parseFloat(priceValue).toFixed(2) * 0.01;
+    return price;
+
+}
 
 function selectOption (liOption) {
     let productSelected = liOption.textContent;
     td_query.innerHTML = productSelected;
+    td_query_price.innerHTML = '$ ' + extractNumber(productSelected);
+    price = extractNumber(productSelected);
     inputBox.value = "";
+    td_query_value.value = "";
     search_Menu.classList.remove("active");
 
     console.log(td_query.innerHTML);
+}
+
+function multiplyPriceAndQuantity() {
+    var quantity = td_query_value.value;
+    var result = quantity * price;
+    td_query_total.innerHTML = '$' + result;
+    console.log(td_query_total.innerHTML);
 }
 
 function showOptions (list) {
