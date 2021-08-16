d3d444e631f76ca353abfb92e0763d2764e3053d
githubGaboDotCom <completamentesud@gmail.com>
2021-08-16 04:19:39 -0600

    Finished adding the code to update the final total

--- calculator_features.js
+++ calculator_features.js
@@ -43,7 +43,7 @@ function selectOption (liOption) {
     price = extractNumber(productSelected);
     var trAdded = document.createElement('tr');
 
-    let tdQuantity = '<input class="products_quantity" type="number" placeholder="0" title="Agrega una cantidad" onkeyup="multiplyPriceAndQuantity(event)" onclick="multiplyPriceAndQuantity(event)">'
+    let tdQuantity = '<input class="products_quantity" type="number" placeholder="0" title="Agrega una cantidad" onkeyup="multiplyPriceAndQuantity()" onclick="multiplyPriceAndQuantity(event)">'
     let removeIcon = '<span class="removeIcon"><i class="fa fa-trash-o" id="trashBin" onclick="addOrRemoveProduct()"></i></span>';
     let tdOptions = '<td>' + productSelected + '</td>' + '<td class="priceValue">' + '$' + price + '</td>'
     + '<td>' + tdQuantity + '</td>' + '<td>' + removeIcon + '</td>' + '<td class="totalRow">' + '$0.00' + '</td>';
@@ -55,31 +55,44 @@ function selectOption (liOption) {
     DropDown_Menu.classList.remove("active");
 }
 
-
 function addOrRemoveProduct () {
     var icon = document.getElementsByClassName('removeIcon');
+    var removedFinalTotal = 0;
 
     for(var i = 0; i < icon.length; i++){
         var iconClicked = icon[i];
         iconClicked.addEventListener('click', function(event){
             var productR = event.target;
             productR.parentElement.parentElement.parentElement.remove();
+            var currentNumOfProducts = document.getElementsByClassName('totalRow');
+            if (currentNumOfProducts.length == 0){
+                document.getElementById('total_value').innerHTML = '$0.00';
+            }
+            for(var r = 0; r < currentNumOfProducts.length; r++){
+                var currentNumOfProducts = document.getElementsByClassName('totalRow');
+                var removedFinalValue = parseFloat(currentNumOfProducts[r].innerHTML.replace('$', ''));
+                removedFinalTotal += removedFinalValue;
+                console.log(removedFinalTotal);
+                document.getElementById('total_value').innerHTML = '$' + removedFinalTotal; 
+        
+            }
         });
     }
 }
 
-function multiplyPriceAndQuantity(event) {
+function multiplyPriceAndQuantity() {
 
-    var input = event.target;
+    var finalTotal = 0;
     var quantity = document.getElementsByClassName("products_quantity");
     var priceV = document.getElementsByClassName("priceValue");
     for(var j = 0; j < quantity.length; j++){
         var finalPrice = parseFloat(priceV[j].innerHTML.replace('$', ''));
         var quantityInput = quantity[j];
-        console.log(quantityInput.value);
-        console.log(finalPrice);
         var result = quantityInput.value * finalPrice;
         document.getElementsByClassName("totalRow")[j].innerHTML = '$' + result;
+        var finalValue = parseFloat(document.getElementsByClassName('totalRow')[j].innerHTML.replace('$', ''));
+        finalTotal += finalValue;
+        document.getElementById('total_value').innerHTML = '$' + finalTotal;
     }
 }
 
